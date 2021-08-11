171e76d39997a82e5b547350eed94122ebbdc503
dudleyneedham <dudleyneedham1993@gmail.com>
2021-08-11 11:58:41 +0200

    feat: minor changes to styling and naming of files and adding to sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,14 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['delegations', 'verification', 'requesting-terms', 'doc1', 'doc2', 'doc3'],
+    Docusaurus: [
+      'ctypes',
+      'delegations',
+      'verification',
+      'requesting-terms',
+      'doc1',
+      'doc2',
+      'doc3',
+    ],
     Features: ['mdx'],
   },
-};
+}
