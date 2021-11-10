7a9379270b5f54b4c0d7f830f8f9c78ba5f3f853
dudleyneedham <dudleyneedham1993@gmail.com>
2021-11-10 11:26:55 +0100

    feat: updating the code exmaples for request for attestation to sign with did

--- code-examples/4_2_claim.ts
+++ code-examples/4_2_claim.ts
@@ -1,13 +1,18 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
+import { KeyringPair } from '@polkadot/keyring/types'
 
-export function main(
-  claimer: Kilt.Identity,
+export async function main(
+  claimer: KeyringPair,
+  claimerLightDid: Kilt.LightDidDetails,
   claim: Kilt.Claim
 ): Kilt.RequestForAttestation {
   const requestForAttestation = Kilt.RequestForAttestation.fromClaimAndIdentity(
     claim,
     claimer
   )
+
+  //
+  await requestForAttestation.signWithDid(claimerLightDid)
 
   // log this so you can paste it locally
   console.log(
