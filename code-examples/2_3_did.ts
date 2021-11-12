dee87cf3d6cc83988609f567298036510c1d8cb5
dudleyneedham <dudleyneedham1993@gmail.com>
2021-11-12 15:12:51 +0100

    feat: updating the creation of the attester full did to make it easier

--- code-examples/2_3_did.ts
+++ code-examples/2_3_did.ts
@@ -7,61 +7,11 @@ export async function main(
 ) {
   await Kilt.connect()
 
-  // Signing keypair
-  const attesterSigningKeypair = await keystore.generateKeypair({
-    alg: Kilt.Did.SigningAlgorithms.Ed25519,
-    seed: attesterMnemonic,
-  })
-
-  // Encryption keypair
-  const attesterEncryptionKeypair = await keystore.generateKeypair({
-    alg: Kilt.Did.EncryptionAlgorithms.NaclBox,
-    seed: attesterMnemonic,
-  })
-
-  const keys: Partial<
-    Record<Kilt.KeyRelationship, Kilt.Did.DidTypes.INewPublicKey<string>>
-  > = {
-    authentication: {
-      publicKey: attesterSigningKeypair.publicKey,
-      type: Kilt.Did.DemoKeystore.getKeypairTypeForAlg(
-        attesterSigningKeypair.alg
-      ),
-    },
-    keyAgreement: {
-      publicKey: attesterEncryptionKeypair.publicKey,
-      type: Kilt.Did.DemoKeystore.getKeypairTypeForAlg(
-        attesterEncryptionKeypair.alg
-      ),
-    },
-    capabilityDelegation: {
-      publicKey: attesterSigningKeypair.publicKey,
-      type: Kilt.Did.DemoKeystore.getKeypairTypeForAlg(
-        attesterSigningKeypair.alg
-      ),
-    },
-    assertionMethod: {
-      publicKey: attesterSigningKeypair.publicKey,
-      type: Kilt.Did.DemoKeystore.getKeypairTypeForAlg(
-        attesterSigningKeypair.alg
-      ),
-    },
-  }
-
-  const { extrinsic, did } = await Kilt.Did.DidUtils.writeDidFromPublicKeys(
+  const attesterFullDid = await Kilt.Did.createOnChainDidFromSeed(
+    attester,
     keystore,
-    attester.address,
-    keys
+    attesterMnemonic
   )
-
-  await Kilt.BlockchainUtils.signAndSubmitTx(extrinsic, attester, {
-    reSign: true,
-    resolveOn: Kilt.BlockchainUtils.IS_FINALIZED,
-  })
-
-  const attesterFullDid = await Kilt.Did.DefaultResolver.resolveDoc(did)
-
-  console.log('Attesters Full DID:', attesterFullDid)
   await Kilt.disconnect()
 
   return [attesterFullDid, keystore]
