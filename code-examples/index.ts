d7494434eac4c64bd9b840a649a4a8d74c75c5ea
dudleyneedham <dudleyneedham1993@gmail.com>
2021-11-12 16:38:40 +0100

    feat: adding the exponent

--- code-examples/index.ts
+++ code-examples/index.ts
@@ -46,10 +46,10 @@ async function test_all() {
   const { claimer, attester, attesterMnemonic } = account2()
 
   await Promise.all([
-    Kilt.Balance.makeTransfer(claimer.address, new BN(100)) //
+    Kilt.Balance.makeTransfer(claimer.address, new BN(100), 15) //
       .then((tx) => Kilt.BlockchainUtils.signAndSubmitTx(tx, faucetAcc))
       .then(() => console.log('Successfully transferred tokens to claimer')),
-    Kilt.Balance.makeTransfer(attester.address, new BN(100)) //
+    Kilt.Balance.makeTransfer(attester.address, new BN(100), 15) //
       .then((tx) => Kilt.BlockchainUtils.signAndSubmitTx(tx, faucetAcc))
       .then(() => console.log('Successfully transferred tokens to attester')),
     ,
