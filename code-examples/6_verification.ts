02cb410e3d840e4bf6e9a33403edb89ae5f4e8de
Skedley <48058984+Dudleyneedham@users.noreply.github.com>
2021-11-15 14:18:53 +0100

    Update code-examples/6_verification.ts
    Co-authored-by: Albrecht <14820950+weichweich@users.noreply.github.com>

--- code-examples/6_verification.ts
+++ code-examples/6_verification.ts
@@ -5,7 +5,7 @@ export async function main(credential: Kilt.Credential) {
   // 1. verifies that the data is valid for the given CTYPE
   // 2. verifies that the attestation hash is present on the Kilt blockchain and that the attestation has not been revoked
   const isValid = await credential.verify()
-  console.log('Is the attested claim valid?', isValid)
+  console.log('Is the credential valid?', isValid)
 
   // disconnect from the chain
   await Kilt.disconnect()
