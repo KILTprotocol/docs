de6efa98fe0ecb841649c72371b418436e39fafc
Timo Welde <tjwelde@gmail.com>
2021-11-16 10:51:24 +0100

    feat: new teaser images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
 import Layout from '@theme/Layout'
+import ThemedImage from '@theme/ThemedImage'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
@@ -10,7 +11,8 @@ const featureRows = [
   [
     {
       title: 'Become a Collator',
-      imageUrl: 'img/community.svg',
+      imageUrl: 'img/community_light.svg',
+      imageUrlDark: 'img/community_dark.svg',
       link: 'docs/chain/collator',
       description: (
         <>
@@ -21,7 +23,8 @@ const featureRows = [
     },
     {
       title: 'Workshop',
-      imageUrl: 'img/expert.svg',
+      imageUrl: 'img/expert_light.svg',
+      imageUrlDark: 'img/expert_dark.svg',
       link: 'docs/sdk/workshop/welcome',
       description: (
         <>
@@ -32,7 +35,8 @@ const featureRows = [
     },
     {
       title: 'Whitepaper',
-      imageUrl: 'img/whitepaper.svg',
+      imageUrl: 'img/whitepaper_light.svg',
+      imageUrlDark: 'img/whitepaper_dark.svg',
       link: 'https://www.kilt.io/wp-content/uploads/2020/01/KILT-White-Paper-v2020-Jan-15.pdf',
       description: (
         <>
@@ -44,14 +48,22 @@ const featureRows = [
   ],
 ]
 
-function Feature({ imageUrl, title, description, link }) {
+function Feature({ imageUrl, imageUrlDark, title, description, link }) {
   const imgUrl = useBaseUrl(imageUrl)
+  const imgUrlDark = useBaseUrl(imageUrlDark)
   return (
     <div className={clsx('col col--4', styles.feature)}>
       <a href={useBaseUrl(link)} className={styles.featureLink}>
         {imgUrl && (
           <div className="text--center">
-            <img className={styles.featureImage} src={imgUrl} alt={title} />
+            <ThemedImage
+              alt={title}
+              className={styles.featureImage}
+              sources={{
+                light: imgUrl,
+                dark: imgUrlDark,
+              }}
+            />
           </div>
         )}
         <h3>{title}</h3>
