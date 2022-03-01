fc22460bf45ae86415a58b080f3908fc80b64525
Antonio Antonino <antonio@kilt.io>
2022-03-01 09:06:30 +0100

    chore: fixed core_features

--- code-examples/core_features/claiming/1_claiming.ts
+++ code-examples/core_features/claiming/1_claiming.ts
@@ -1,6 +1,6 @@
 import { CType } from '@kiltprotocol/core'
 
-export async function main() {
+export async function main(): Promise<CType> {
   const ctype = CType.fromSchema({
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
     title: 'Drivers License',
--- code-examples/core_features/did/index.ts
+++ code-examples/core_features/did/index.ts
@@ -1,9 +1,8 @@
 import { randomAsHex } from '@polkadot/util-crypto'
 
-import { BlockchainUtils } from '@kiltprotocol/chain-helpers'
 import { init as kiltInit } from '@kiltprotocol/core'
-import { FullDidDetails, DemoKeystore, DefaultResolver } from '@kiltprotocol/did'
-import * as Kilt from '@kiltprotocol/sdk-js'
+import { DemoKeystore } from '@kiltprotocol/did'
+import { Keyring } from '@kiltprotocol/utils'
 
 import { main as main1 } from './1_did'
 import { main as main2 } from './2_did'
@@ -19,59 +18,46 @@ const SEED_ENV = 'FAUCET_SEED'
 export async function runAll() {
   await kiltInit({ address: 'wss://peregrine.kilt.io/parachain-public-ws' })
 
-  let keyring = new Kilt.Utils.Keyring({
+  const keyring = new Keyring({
     type: 'sr25519',
     ss58Format: 38,
   })
+  const keystore = new DemoKeystore()
 
   const faucetSeed = process.env[SEED_ENV]
-  if (faucetSeed === undefined) {
-    console.log(
-      `Account seed with sufficient balance is required. Set the secret seed using the ${SEED_ENV} environment variable.`
-    )
-    throw 'Account seed is missing'
+  if (!faucetSeed) {
+    throw `Account seed with sufficient balance is required. Set the secret seed using the ${SEED_ENV} environment variable.`
   }
 
   const faucetAccount = keyring.createFromUri(faucetSeed)
 
-  const keystore = new DemoKeystore()
-
   console.log('main1 - create light DID')
-  await main1()
+  await main1(keystore)
   console.log('main2 - create light DID with encryption key')
-  await main2()
+  await main2(keystore)
 
   console.log('main3 - create full DID')
   const randomMini3 = randomAsHex(32)
-  const did3 = await main3(keystore, faucetAccount, BlockchainUtils.IS_IN_BLOCK, randomMini3)
+  await main3(keystore, faucetAccount, randomMini3)
 
   console.log('main4 - create full DID with encryption key and service endpoints')
   const authSeed4 = randomAsHex(32)
   const encSeed4 = randomAsHex(32)
-  const did4 = await main4(keystore, faucetAccount, BlockchainUtils.IS_IN_BLOCK, authSeed4, encSeed4)
+  const did4 = await main4(keystore, faucetAccount, authSeed4, encSeed4)
 
   console.log('main5 - update auth key, remove service endpoint')
   const randomMini5 = randomAsHex(32)
-  await main5(keystore, faucetAccount, BlockchainUtils.IS_IN_BLOCK, randomMini5, did4.details as FullDidDetails)
+  const updatedDidDetails = await main5(keystore, faucetAccount, randomMini5, did4)
 
   console.log('main6 - delete DID')
-  let did4_identifier = did4.details?.did
-  if (typeof did4_identifier === 'undefined') {
-    throw 'The did cannot be deleted, we just created it'
-  }
-  // Get the updated DID Doc
-  const updatedDidDetails4 = (await (await DefaultResolver.resolveDoc(did4_identifier))?.details) as FullDidDetails
-  if (updatedDidDetails4 === undefined) {
-    throw 'We just created the did'
-  }
-  await main6(keystore, faucetAccount, BlockchainUtils.IS_IN_BLOCK, updatedDidDetails4)
+  await main6(keystore, faucetAccount, updatedDidDetails)
 
   console.log('main7 - claim DID deposit')
-  await main7(faucetAccount, BlockchainUtils.IS_IN_BLOCK, did3.details as FullDidDetails)
+  await main7(faucetAccount, updatedDidDetails.identifier)
 
   console.log('main8 - upgrade light DID')
   const randomMini8 = randomAsHex(32)
-  const did8 = await main8(keystore, faucetAccount, BlockchainUtils.IS_IN_BLOCK, randomMini8)
+  const did8 = await main8(keystore, faucetAccount, randomMini8)
   console.log('main7 - again, delete DID from main8')
-  await main7(faucetAccount, BlockchainUtils.IS_IN_BLOCK, did8.details as FullDidDetails)
+  await main7(faucetAccount, did8.identifier)
 }
