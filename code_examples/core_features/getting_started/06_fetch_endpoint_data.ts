d35c3eabda6fc6dc4d8bba5955f34608a3546fd5
Timo Welde <tjwelde@gmail.com>
2022-05-30 17:19:10 +0200

    refactor(getting-started): no any & add FIXME comment

--- code_examples/core_features/getting_started/06_fetch_endpoint_data.ts
+++ code_examples/core_features/getting_started/06_fetch_endpoint_data.ts
@@ -3,9 +3,11 @@ import axios from 'axios'
 import * as Kilt from '@kiltprotocol/sdk-js'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function main(endpoints: Kilt.DidServiceEndpoint[]): Promise<any> {
+export async function main(
+  endpoints: Kilt.DidServiceEndpoint[]
+): Promise<Kilt.IRequestForAttestation> {
   const endpointRequestData = await axios
-    .get(endpoints[0].urls[0])
+    .get<Kilt.IRequestForAttestation>(endpoints[0].urls[0])
     .then((response) => response.data)
 
   return endpointRequestData
