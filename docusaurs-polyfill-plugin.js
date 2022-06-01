3935989d39cc3d5ab27efed6bc21fd6475e6f969
Timo Welde <tjwelde@gmail.com>
2022-06-01 10:47:26 +0200

    fix: need to shim process

--- docusaurs-polyfill-plugin.js
+++ docusaurs-polyfill-plugin.js
@@ -1,18 +1,31 @@
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = function (context, options) {
   return {
     name: 'docusaurs-polyfill-plugin',
     configureWebpack(config, isServer, utils) {
       return {
+        module: {
+          rules: [
+            {
+              // Needed for making the `process/browser` in the ProvidePlugin work.
+              test: /\.m?js/,
+              include: path.resolve(__dirname, 'node_modules'),
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
+        },
         resolve: {
           fallback: {
             stream: require.resolve('stream-browserify'),
           },
         },
         plugins: [
-          new webpack.DefinePlugin({
-            'process.env.DEBUG': false,
+          new webpack.ProvidePlugin({
+            process: 'process/browser',
           }),
         ],
       }
