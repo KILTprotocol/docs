49b424cc8ae0243767d032f88bb2da71503df688
Timo Welde <tjwelde@gmail.com>
2022-06-10 13:53:38 +0200

    feat: new meta image and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
   return (
     <Layout
       title="Docs"
-      description="KILT Protocol documentation to learn about KILT concepts, how to built on KILT and how to participate."
+      description="KILT Protocol documentation to learn about KILT concepts, how to built on KILT and how to participate in the KILT ecosystem."
     >
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
