fe9bf2c46cf2b069b69775100077e4aa588395c4
Antonio Antonino <antonio@kilt.io>
2022-07-27 10:50:46 +0200

    feat: add text support to JsonSnippet component

--- src/components/JsonSnippet/index.js
+++ src/components/JsonSnippet/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import CodeBlock from '@theme/CodeBlock'
 
-export default function JsonSnippet({ obj, spacing = 2 }) {
+export default function JsonSnippet({ obj, text, spacing = 2 }) {
   return (
-    <CodeBlock className="language-json">{JSON.stringify(obj, null, spacing)}</CodeBlock>
+    <CodeBlock className="language-json" text={text}>{JSON.stringify(obj, null, spacing)}</CodeBlock>
   )
 }
