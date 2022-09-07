6c387c9a3abc41fce405fa490b71600d9f9bc03d
Antonio Antonino <antonio@kilt.io>
2022-09-07 15:32:10 +0200

    Styling

--- code_examples/core_features/getting_started/07_verify_attestation.ts
+++ code_examples/core_features/getting_started/07_verify_attestation.ts
@@ -1,6 +1,8 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
 
 export async function main(credential: Kilt.ICredential): Promise<boolean> {
-  const isAttestationValid = await Kilt.Attestation.checkValidity(credential.rootHash)
+  const isAttestationValid = await Kilt.Attestation.checkValidity(
+    credential.rootHash
+  )
   return isAttestationValid
 }
