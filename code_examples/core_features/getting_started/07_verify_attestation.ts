c0e031d27910440dfcecf9247975682c7e6425b9
Antonio Antonino <antonio@kilt.io>
2022-09-07 15:31:49 +0200

    Add step to verify credential attestation

---
+++ code_examples/core_features/getting_started/07_verify_attestation.ts
@@ -0,0 +1,6 @@
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+export async function main(credential: Kilt.ICredential): Promise<boolean> {
+  const isAttestationValid = await Kilt.Attestation.checkValidity(credential.rootHash)
+  return isAttestationValid
+}
--- code_examples/core_features/getting_started/index.ts
+++ code_examples/core_features/getting_started/index.ts
@@ -1,13 +1,14 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
 
 import { main as connect } from './03_connect'
-import { main as disconnect } from './08_disconnect'
+import { main as disconnect } from './09_disconnect'
 import { main as fetchEndpointData } from './06_fetch_endpoint_data'
 import { main as fetchJohnDoeDid } from './04_fetch_did'
 import { main as fetchJohnDoeEndpoints } from './05_fetch_endpoints'
 import { main as initSDKWithSpiritnet } from './02_init_sdk'
 import { main as printHelloWorld } from './01_print_hello_world'
-import { main as verifyCredential } from './07_verify_credential'
+import { main as verifyAttestation } from './07_verify_attestation'
+import { main as verifyCredential } from './08_verify_credential'
 
 export async function runAll(): Promise<void> {
   printHelloWorld()
@@ -29,6 +30,13 @@ export async function runAll(): Promise<void> {
     // For now simply disconnect and return (i.e., ignore this error).
     await disconnect()
     return
+  }
+
+  const attestationStatus = await verifyAttestation(credential)
+  if (attestationStatus) {
+    console.log("John Doe's credential is attested!")
+  } else {
+    console.log("John Doe's credential is not attested.")
   }
 
   await verifyCredential(credential)
