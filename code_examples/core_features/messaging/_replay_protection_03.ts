f1ff50c8aba3c0525112e7dcfc16ada3aac414f5
Antonio Antonino <antonio@kilt.io>
2022-09-14 11:00:37 +0200

    Minor fixes

--- code_examples/core_features/messaging/_replay_protection_03.ts
+++ code_examples/core_features/messaging/_replay_protection_03.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-export function main(submissions: Map<string, number>, MAX_ACCEPTED_AGE: number) {
+export function main(
+  submissions: Map<string, number>,
+  MAX_ACCEPTED_AGE: number
+) {
   setInterval(() => {
     const outdatedTimestamp = Date.now() - MAX_ACCEPTED_AGE
     submissions.forEach((timestamp, hash) => {
