b3db3c8eb9970df0182c0c4182c7584961fc20a6
Antonio <antonio@kilt.io>
2022-09-16 08:42:09 +0200

    feat: update snippet to use the format for published credentials (#152)
    * feat: update snippet to use the format for published credentials
    
    * Style fix
    
    * Fix for web3name credentials
    
    * Fix types

--- code_examples/core_features/getting_started/06_fetch_endpoint_data.ts
+++ code_examples/core_features/getting_started/06_fetch_endpoint_data.ts
@@ -1,14 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios'
 
 import * as Kilt from '@kiltprotocol/sdk-js'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// TODO: Use exported type when https://github.com/KILTprotocol/sdk-js/pull/631 is merged.
+
 export async function main(
   endpoints: Kilt.DidServiceEndpoint[]
 ): Promise<Kilt.IRequestForAttestation> {
-  const endpointRequestData = await axios
-    .get<Kilt.IRequestForAttestation>(endpoints[0].urls[0])
-    .then((response) => response.data)
+  // Define the type of a published credential
+  type PublishedCredential = {
+    credential: Kilt.IRequestForAttestation
+    // Not relevant for this case, but production applications should parse this field as well
+    metadata?: any
+  }
+  const {
+    data: [{ credential }]
+  } = await axios.get<PublishedCredential[]>(endpoints[1].urls[0])
 
-  return endpointRequestData
+  return credential
 }
