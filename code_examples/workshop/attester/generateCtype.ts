34f603e4b8308f27b9224696e3defd005b925caa
Niklas P <niklasp@users.noreply.github.com>
2022-11-07 15:17:58 +0100

    Update generateCtype.ts
    nicer output format

--- code_examples/workshop/attester/generateCtype.ts
+++ code_examples/workshop/attester/generateCtype.ts
@@ -58,7 +58,7 @@ if (require.main === module) {
         keyType: attestation.type
       }))
       
-      console.log(`your ctype was succsesfully created\n\n${ JSON.stringify( newCType ) }`)
+      console.log(`your ctype was succsesfully created\n\n${ JSON.stringify( newCType, null, 2 ) }`)
     } catch (e) {
       console.log('Error while checking on chain ctype')
       throw e
