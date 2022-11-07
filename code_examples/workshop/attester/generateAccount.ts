399e2329860d56bf7bbc00f2cf999f229d02ca60
Niklas P <niklasp@users.noreply.github.com>
2022-11-07 14:48:49 +0100

    Update generateAccount.ts (#175)
    Add missing exclamation marks for copy & paste into .env

--- code_examples/workshop/attester/generateAccount.ts
+++ code_examples/workshop/attester/generateAccount.ts
@@ -26,7 +26,7 @@ if (require.main === module) {
       const { mnemonic, account } = generateAccount()
       console.log('save to mnemonic and address to .env to continue!\n\n')
       console.log(`ATTESTER_ACCOUNT_MNEMONIC="${mnemonic}"`)
-      console.log(`ATTESTER_ACCOUNT_ADDRESS=${account.address}\n\n`)
+      console.log(`ATTESTER_ACCOUNT_ADDRESS="${account.address}"\n\n`)
     } catch (e) {
       console.log('Error while setting up attester account')
       throw e
