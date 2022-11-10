e4cc35ea0bdd7566ad0897d507f23c0c9cfe8457
Antonio <antonio@kilt.io>
2022-11-10 16:11:39 +0100

    Update getting started example (#180)

--- code_examples/core_features/getting_started/03_fetch_did.ts
+++ code_examples/core_features/getting_started/03_fetch_did.ts
@@ -2,9 +2,8 @@ import type { ApiPromise } from '@polkadot/api'
 
 import * as Kilt from '@kiltprotocol/sdk-js'
 
-// TODO: Replace api.call.didApi with api.call.did once it's deployed on Spiritnet
 export async function main(api: ApiPromise): Promise<Kilt.DidUri | null> {
-  const encodedJohnDoeDetails = await api.call.didApi.queryDidByW3n('john_doe')
+  const encodedJohnDoeDetails = await api.call.did.queryByWeb3Name('john_doe')
 
   // This function will throw if johnDoeOwner does not exist
   const {
