7d340c5505ddf546f93f664377a6c615c2745b20
Skedley <48058984+Dudleyneedham@users.noreply.github.com>
2023-01-04 11:20:41 +0100

    fix quickstart (#193)
    * fix: including the init the project
    
    * feat: upgrading to latest docs packages
    
    * feat: include the configservice get api
    
    * feat: updatin the quickstart guide
    
    * fix: removed the main
    
    * fix: lintiing issues
    
    * Apply suggestions from code review
    
    Co-authored-by: Antonio <antonio@kilt.io>
    
    * feat: updating as per the comments of review
    
    * feat: changing the console log
    
    Co-authored-by: Antonio <antonio@kilt.io>

--- code_examples/core_features/getting_started/02_connect.ts
+++ code_examples/core_features/getting_started/02_connect.ts
@@ -3,5 +3,9 @@ import type { ApiPromise } from '@polkadot/api'
 import * as Kilt from '@kiltprotocol/sdk-js'
 
 export async function main(): Promise<ApiPromise> {
-  return Kilt.connect('wss://spiritnet.kilt.io/')
+  await Kilt.connect('wss://spiritnet.kilt.io/')
+
+  const api = Kilt.ConfigService.get('api')
+
+  return api
 }
