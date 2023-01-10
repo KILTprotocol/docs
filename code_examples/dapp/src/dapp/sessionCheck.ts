a056fc80cad30971c4c26913c0c0994a7fcdb369
weichweich <albrecht@kilt.io>
2023-01-10 13:07:30 +0100

    style...

--- code_examples/dapp/src/dapp/sessionCheck.ts
+++ code_examples/dapp/src/dapp/sessionCheck.ts
@@ -14,7 +14,7 @@ export async function main() {
   const { encryptionKeyUri, encryptedChallenge, nonce } = session
   const encryptionKey = await Kilt.Did.resolveKey(encryptionKeyUri)
   if (!encryptionKey) {
-    throw "an encryption key is required"
+    throw 'an encryption key is required'
   }
 
   const decryptedBytes = Kilt.Utils.Crypto.decryptAsymmetric(
