cd0a1a0e6a9b290eb6a4e721ae16d4cbf82f35db
weichweich <albrecht@kilt.io>
2023-01-13 09:40:32 +0100

    fix formatting

--- code_examples/sdk_examples/src/dapp/dapp/formatCredential.ts
+++ code_examples/sdk_examples/src/dapp/dapp/formatCredential.ts
@@ -39,21 +39,23 @@ export async function main({
 
   const wellKnownDidconfig = {
     '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
-    linked_dids: [{
-      "@context": [
-        "https://www.w3.org/2018/credentials/v1",
-        "https://identity.foundation/.well-known/did-configuration/v1"
-      ],
-      issuer,
-      issuanceDate,
-      type: [
-        'VerifiableCredential',
-        'DomainLinkageCredential',
-        'KiltCredential2020'
-      ],
-      credentialSubject,
-      proof
-    }]
+    linked_dids: [
+      {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          'https://identity.foundation/.well-known/did-configuration/v1'
+        ],
+        issuer,
+        issuanceDate,
+        type: [
+          'VerifiableCredential',
+          'DomainLinkageCredential',
+          'KiltCredential2020'
+        ],
+        credentialSubject,
+        proof
+      }
+    ]
   }
 
   console.log(JSON.stringify(wellKnownDidconfig))
