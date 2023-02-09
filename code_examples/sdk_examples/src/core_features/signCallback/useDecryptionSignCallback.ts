1dfdeaad0dec8d14f1aca91f6ed94ee6f4cd5c10
dudleyneedham <dudleyneedham1993@gmail.com>
2023-02-09 12:47:32 +0100

    feat: adding encryp and decrypt callbacks

---
+++ code_examples/sdk_examples/src/core_features/signCallback/useDecryptionSignCallback.ts
@@ -0,0 +1,29 @@
+import * as Kilt from '@kiltprotocol/sdk-js'
+import { naclOpen } from '@polkadot/util-crypto'
+
+export function useDecryptionSignCallback({
+  keyAgreement
+}: {
+  keyAgreement: Kilt.KiltEncryptionKeypair
+}): Kilt.DecryptCallback {
+  return async function decryptCallback({
+    data,
+    nonce,
+    peerPublicKey
+  }): Promise<Kilt.DecryptResponseData> {
+    const decrypted = naclOpen(
+      data,
+      nonce,
+      peerPublicKey,
+      keyAgreement.secretKey
+    )
+
+    if (!decrypted) {
+      throw new Error('Failed to decrypt with given key')
+    }
+
+    return {
+      data: decrypted
+    }
+  }
+}
---
+++ code_examples/sdk_examples/src/core_features/signCallback/useEncryptionSignCallback.ts
@@ -0,0 +1,30 @@
+import * as Kilt from '@kiltprotocol/sdk-js'
+import { naclSeal } from '@polkadot/util-crypto'
+
+export function useEncryptionSignCallback({
+  keyAgreement,
+  didDocument
+}: {
+  keyAgreement: Kilt.KiltEncryptionKeypair
+  didDocument: Kilt.DidDocument
+}): Kilt.EncryptCallback {
+  return async function encryptCallback({
+    data,
+    peerPublicKey
+  }): Promise<Kilt.EncryptResponseData> {
+    const keyId = didDocument.keyAgreement?.[0].id
+    if (!keyId) {
+      throw new Error(`Encryption key not found in did "${didDocument.uri}"`)
+    }
+    const { sealed, nonce } = naclSeal(
+      data,
+      peerPublicKey,
+      keyAgreement.secretKey
+    )
+    return {
+      nonce,
+      data: sealed,
+      keyUri: `${didDocument.uri}${keyId}`
+    }
+  }
+}
