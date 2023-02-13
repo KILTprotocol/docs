d8f5cd8ffd5cd83feb29157da3598223ec71ab2b
dudleyneedham <dudleyneedham1993@gmail.com>
2023-02-13 10:52:09 +0100

    feat: more verbose naming in test

--- code_examples/sdk_examples/src/core_features/messaging/index.ts
+++ code_examples/sdk_examples/src/core_features/messaging/index.ts
@@ -8,48 +8,48 @@ import { generateMessage } from './01_generateMessage'
 // Runs through the messaging encryption and decryption of messages
 export async function runAll(submitterAccount: Kilt.KiltKeyringPair) {
   console.log('Running messaging flow...')
-  const user1 = generateKeypairs()
-  const fullDidUser1 = await createCompleteFullDid(
+  const sender = generateKeypairs()
+  const senderFullDid = await createCompleteFullDid(
     submitterAccount,
     {
-      ...user1
+      ...sender
     },
     async ({ data }) => ({
-      signature: user1.authentication.sign(data),
-      keyType: user1.authentication.type
+      signature: sender.authentication.sign(data),
+      keyType: sender.authentication.type
     })
   )
 
-  const user2 = generateKeypairs()
-  const fullDidUser2 = await createCompleteFullDid(
+  const receiver = generateKeypairs()
+  const receiverFullDid = await createCompleteFullDid(
     submitterAccount,
     {
-      ...user2
+      ...receiver
     },
     async ({ data }) => ({
-      signature: user2.authentication.sign(data),
-      keyType: user2.authentication.type
+      signature: receiver.authentication.sign(data),
+      keyType: receiver.authentication.type
     })
   )
   console.log('Generating the message to encrypt and decrypt')
-  const message = await generateMessage(fullDidUser1.uri, fullDidUser2.uri)
+  const message = await generateMessage(senderFullDid.uri, receiverFullDid.uri)
 
   console.log(message)
 
   console.log('Encrypting the message from two users')
   const encryptedMessage = await encryptMessage(
     message,
-    fullDidUser1.uri,
-    fullDidUser2.uri,
-    user1.encryption
+    senderFullDid.uri,
+    receiverFullDid.uri,
+    sender.encryption
   )
 
   console.log(encryptedMessage)
 
   console.log('Decrypting the message from two users')
   const decryptedMessage = await decryptMessage(
-    user2.encryption,
-    encryptedMessage
+    encryptedMessage,
+    receiver.encryption
   )
 
   console.log(decryptedMessage)
