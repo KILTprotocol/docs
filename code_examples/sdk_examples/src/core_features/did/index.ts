c1feaede66c4120d672c8004e87ca965868c2ce1
Albrecht <albrecht@kilt.io>
2023-02-21 11:16:41 +0100

    feat: better explain signing (#183)
    Co-authored-by: Skedley <48058984+Dudleyneedham@users.noreply.github.com>
    Co-authored-by: Antonio <antonio@kilt.io>

--- code_examples/sdk_examples/src/core_features/did/index.ts
+++ code_examples/sdk_examples/src/core_features/did/index.ts
@@ -2,7 +2,6 @@ import { stringToU8a } from '@polkadot/util'
 
 import * as Kilt from '@kiltprotocol/sdk-js'
 
-import { batchCTypeCreationExtrinsics } from './08_full_did_batch'
 import { createCompleteFullDid } from './05_full_did_complete'
 import { createCompleteLightDid } from './02_light_did_complete'
 import { createSimpleFullDid } from './04_full_did_simple'
@@ -11,8 +10,10 @@ import { deleteFullDid } from './10_full_did_delete'
 import { exportDid } from './11_did_export'
 import { generateAndVerifyDidAuthenticationSignature } from './09_did_signature'
 import { migrateLightDid } from './03_light_did_migrate'
-import { queryFullDid } from './06_full_did_query'
+import { queryFullDid } from './06_did_query'
 import { reclaimFullDidDeposit } from './12_full_did_deposit_reclaim'
+import { signAndSubmitDidExtrinsic } from './08_full_did_tx'
+import { signAndSubmitDidExtrinsicBatch } from './08_full_did_batch'
 import { updateFullDid } from './07_full_did_update'
 
 import { generateKeypairs } from '../utils/generateKeypairs'
@@ -75,6 +76,7 @@ export async function runAll(
   )
   console.log('6 did) Query full DID')
   queryFullDid(createdCompleteFullDid.uri)
+
   console.log('7 did) Update full DID created at step 5')
   const { authentication: newCompleteFullDidAuth } = generateKeypairs()
   const updatedFullDid = await updateFullDid(
@@ -87,9 +89,9 @@ export async function runAll(
     })
   )
   console.log(
-    '8 did) Use the same full DID created at step 5 to sign the batch'
+    '8.1 did) Use the same full DID created at step 5 to sign the batch'
   )
-  await batchCTypeCreationExtrinsics(
+  await signAndSubmitDidExtrinsicBatch(
     submitterAccount,
     updatedFullDid.uri,
     async ({ data }) => ({
@@ -97,14 +99,27 @@ export async function runAll(
       keyType: completeFullDidAtt.type
     })
   )
+
+  console.log(
+    '8.2 did) Use the same full DID created at step 5 to sign the single tx'
+  )
+  await signAndSubmitDidExtrinsic(
+    submitterAccount,
+    updatedFullDid.uri,
+    async ({ data }) => ({
+      signature: completeFullDidAtt.sign(data),
+      keyType: completeFullDidAtt.type
+    })
+  )
+
   console.log(
     '9 did) Use the same full DID created at step 5 to generate the signature'
   )
   await generateAndVerifyDidAuthenticationSignature(
     updatedFullDid,
     stringToU8a('test-payload'),
-    async ({ data }) => ({
-      signature: newCompleteFullDidAuth.sign(data),
+    async () => ({
+      key: newCompleteFullDidAuth,
       keyType: newCompleteFullDidAuth.type,
       keyUri: `${updatedFullDid.uri}${updatedFullDid.authentication[0].id}`
     })
