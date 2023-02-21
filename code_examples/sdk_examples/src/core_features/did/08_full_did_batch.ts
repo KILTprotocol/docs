3d26753f9772fded0ff7a6f8af256f707ca2b25a
Albrecht <albrecht@kilt.io>
2023-02-21 11:16:41 +0100

    feat: better explain signing (#183)
    Co-authored-by: Skedley <48058984+Dudleyneedham@users.noreply.github.com>
    Co-authored-by: Antonio <antonio@kilt.io>

--- code_examples/sdk_examples/src/core_features/did/08_full_did_batch.ts
+++ code_examples/sdk_examples/src/core_features/did/08_full_did_batch.ts
@@ -1,40 +1,27 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
+// Just a helper to get an extrinsic
+import getExtrinsic from '../utils/getExtrinsic'
 
-function getRandomCType(): Kilt.ICType {
-  // Random factor ensures that each created CType is unique and does not already exist on chain.
-  const randomFactor = Kilt.Utils.UUID.generate()
-  return Kilt.CType.fromProperties(`CType ${randomFactor}`, {
-    name: {
-      type: 'string'
-    },
-    age: {
-      type: 'integer'
-    }
-  })
-}
-
-export async function batchCTypeCreationExtrinsics(
+export async function signAndSubmitDidExtrinsicBatch(
   submitterAccount: Kilt.KiltKeyringPair,
   fullDid: Kilt.DidUri,
   signCallback: Kilt.SignExtrinsicCallback
 ): Promise<void> {
   const api = Kilt.ConfigService.get('api')
 
-  // Create two random demo CTypes.
-  const ctype1 = getRandomCType()
-  const ctype1CreationTx = api.tx.ctype.add(Kilt.CType.toChain(ctype1))
-  const ctype2 = getRandomCType()
-  const ctype2CreationTx = api.tx.ctype.add(Kilt.CType.toChain(ctype2))
+  // Build two extrinsics
+  const extrinsic1 = getExtrinsic()
+  const extrinsic2 = getExtrinsic()
 
   // Create the DID-signed batch.
   const authorizedBatch = await Kilt.Did.authorizeBatch({
     batchFunction: api.tx.utility.batchAll,
     did: fullDid,
-    extrinsics: [ctype1CreationTx, ctype2CreationTx],
+    extrinsics: [extrinsic1, extrinsic2],
     sign: signCallback,
     submitter: submitterAccount.address
   })
 
-  // The authorized account submits the batch to the chain.
+  // Wrap the DID extrinsic in an account extrinsic.
   await Kilt.Blockchain.signAndSubmitTx(authorizedBatch, submitterAccount)
 }
--- code_examples/sdk_examples/src/core_features/did/09_did_signature.ts
+++ code_examples/sdk_examples/src/core_features/did/09_did_signature.ts
@@ -1,20 +1,32 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
+import { u8aToHex } from '@polkadot/util'
+
+type KeyLookup = (parameter: {
+  didUri: Kilt.DidUri
+  keyRelationship: Kilt.VerificationKeyRelationship
+}) => Promise<{
+  key: Kilt.KiltKeyringPair
+  keyType: Kilt.VerificationKeyType
+  keyUri: Kilt.DidResourceUri
+}>
 
 export async function generateAndVerifyDidAuthenticationSignature(
   did: Kilt.DidDocument,
   payload: Uint8Array,
-  signCallback: Kilt.SignCallback
+  keyLookup: KeyLookup
 ): Promise<void> {
-  // Generate a signature using the provided signing callback.
-  const { signature, keyUri } = await signCallback({
-    data: payload,
-    did: did.uri,
+  // How the key is looked up depends on where the key is stored (e.g. memory, hardware wallet, browser extension)
+  const { key, keyUri } = await keyLookup({
+    didUri: did.uri,
     keyRelationship: 'authentication'
   })
 
+  // Generate a signature using the key that we just looked up.
+  const signature = key.sign(payload)
+
   // Print the generated signature object.
   console.log('Generated signature:')
-  console.log(JSON.stringify(signature, null, 2))
+  console.log(u8aToHex(signature))
 
   // Verify the validity of the signature using the DID's authentication public key.
   // It throws if the signature cannot be verified.
