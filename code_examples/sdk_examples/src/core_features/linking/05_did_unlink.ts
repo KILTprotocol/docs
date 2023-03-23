6f191705232e05a89bddbe8efc4f18724f82b673
Tino Rusch <tino@kilt.io>
2023-03-23 13:11:03 +0100

    prettier

--- code_examples/sdk_examples/src/core_features/linking/05_did_unlink.ts
+++ code_examples/sdk_examples/src/core_features/linking/05_did_unlink.ts
@@ -11,8 +11,9 @@ export async function unlinkAccountFromDid(
   const api = Kilt.ConfigService.get('api')
 
   // The DID owner removes the link between itself and the specified account.
-  const accountUnlinkTx =
-    api.tx.didLookup.removeAccountAssociation({AccountId32: linkedAccountAddress})
+  const accountUnlinkTx = api.tx.didLookup.removeAccountAssociation({
+    AccountId32: linkedAccountAddress
+  })
   const authorizedAccountUnlinkTx = await Kilt.Did.authorizeTx(
     did,
     accountUnlinkTx,
