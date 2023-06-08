3c0c3595d306fbfb04fc8d12987c13093c53be97
Raphael Flechtner <raphael@kilt.io>
2023-06-08 14:40:14 +0200

    refactor: remove verify_attestation from quickstart

--- code_examples/sdk_examples/src/core_features/getting_started/index.ts
+++ code_examples/sdk_examples/src/core_features/getting_started/index.ts
@@ -6,7 +6,6 @@ import { main as fetchEndpointData } from './05_fetch_endpoint_data'
 import { main as fetchJohnDoeDid } from './03_fetch_did'
 import { main as fetchJohnDoeEndpoints } from './04_fetch_endpoints'
 import { main as printHelloWorld } from './01_print_hello_world'
-import { main as verifyAttestation } from './07_verify_attestation'
 import { main as verifyCredential } from './06_verify_credential'
 
 export async function runAll(): Promise<void> {
@@ -29,13 +28,6 @@ export async function runAll(): Promise<void> {
     // For now simply disconnect and return (i.e., ignore this error).
     await disconnect()
     return
-  }
-
-  const attestationStatus = await verifyAttestation(api, credential)
-  if (attestationStatus) {
-    console.log("John Doe's credential is attested!")
-  } else {
-    console.log("John Doe's credential is not attested.")
   }
 
   await verifyCredential(credential)
