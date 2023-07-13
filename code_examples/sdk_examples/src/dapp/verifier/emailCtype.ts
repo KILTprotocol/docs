24396eef17081e1e8d0e122d0839e9c9fd8c114e
dudleyneedham <dudleyneedham1993@gmail.com>
2023-07-13 13:06:57 +0200

    fix: updating the linting

--- code_examples/sdk_examples/src/dapp/verifier/emailCtype.ts
+++ code_examples/sdk_examples/src/dapp/verifier/emailCtype.ts
@@ -4,7 +4,8 @@ import * as Kilt from '@kiltprotocol/sdk-js'
 export function main() {
   const emailCType: Kilt.ICType = {
     $id: 'kilt:ctype:0x3291bb126e33b4862d421bfaa1d2f272e6cdfc4f96658988fbcffea8914bd9ac',
-    $schema: 'ipfs://bafybeiah66wbkhqbqn7idkostj2iqyan2tstc4tpqt65udlhimd7hcxjyq/',
+    $schema:
+      'ipfs://bafybeiah66wbkhqbqn7idkostj2iqyan2tstc4tpqt65udlhimd7hcxjyq/',
     title: 'Email',
     properties: {
       Email: {
