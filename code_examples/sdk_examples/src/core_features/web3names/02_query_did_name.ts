11f6df014cd9b94c625c545b9e49badfc3d157a2
weichweich <albrecht@kilt.io>
2023-07-18 09:20:15 +0200

    add snipped to query DID using a W3N

--- code_examples/sdk_examples/src/core_features/web3names/02_query_did_name.ts
+++ code_examples/sdk_examples/src/core_features/web3names/02_query_did_name.ts
@@ -1,21 +1,20 @@
-import { assert } from 'console'
-
 import * as Kilt from '@kiltprotocol/sdk-js'
 
-export async function verifyNameAndDidEquality(
-  web3Name: Kilt.Did.Web3Name,
-  did: Kilt.DidUri
-): Promise<void> {
+export async function queryDidDocument(
+  web3Name: Kilt.Did.Web3Name
+): Promise<Kilt.DidDocument> {
   const api = Kilt.ConfigService.get('api')
 
   console.log(
-    `Querying the blockchain for the web3name "${web3Name}" and the DID "${did}"...`
+    `Querying the blockchain for the web3name "${web3Name}"`
   )
   // Query the owner of the provided web3name.
   const encodedWeb3NameOwner = await api.call.did.queryByWeb3Name(web3Name)
+
+  // Extract the DidDocument and other linked information from the encodedWeb3NameOwner.
   const {
-    document: { uri }
+    document,
   } = Kilt.Did.linkedInfoFromChain(encodedWeb3NameOwner)
-  // Assert that it is the right owner.
-  assert(uri === did)
+
+  return document
 }
