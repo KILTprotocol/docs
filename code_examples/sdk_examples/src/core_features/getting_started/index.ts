a1f64972edc0d7b6a8a5f7262bbd432beb009ad8
Albrecht <albrecht@kilt.io>
2023-07-27 09:45:49 +0200

    mention peregrine in quickstart guide (#262)

--- code_examples/sdk_examples/src/core_features/getting_started/index.ts
+++ code_examples/sdk_examples/src/core_features/getting_started/index.ts
@@ -1,6 +1,8 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
+import { ApiPromise } from '@polkadot/api'
 
-import { main as connect } from './02_connect'
+import { main as connect_pere } from './02_connect_spirit'
+import { main as connect_spirit } from './02_connect_spirit'
 import { main as disconnect } from './07_disconnect'
 import { main as fetchEndpointData } from './05_fetch_endpoint_data'
 import { main as fetchJohnDoeDid } from './03_fetch_did'
@@ -8,10 +10,7 @@ import { main as fetchJohnDoeEndpoints } from './04_fetch_endpoints'
 import { main as printHelloWorld } from './01_print_hello_world'
 import { main as verifyCredential } from './06_verify_credential'
 
-export async function runAll(): Promise<void> {
-  await printHelloWorld()
-  // Connect to Spiritnet.
-  const api = await connect()
+async function fetchDidAndCredential(api: ApiPromise) {
   const johnDoeDid = await fetchJohnDoeDid(api)
   if (!johnDoeDid)
     throw new Error('"john_doe" is not associated to any DID on Spiritnet')
@@ -21,15 +20,32 @@ export async function runAll(): Promise<void> {
 
   let credential: Kilt.ICredential
   try {
-    credential = await fetchEndpointData(endpoints)
-  } catch {
     // FIXME: Occasionally there is a timeout error, because the endpoint uses the official ipfs gateway.
     // Fix it by using a reliable endpoint.
     // For now simply disconnect and return (i.e., ignore this error).
-    await disconnect()
+    credential = await fetchEndpointData(endpoints)
+  } catch (error) {
+    console.error('Error while fetching IPFS', error)
     return
   }
-
   await verifyCredential(credential)
-  await disconnect()
+}
+
+export async function runAll(): Promise<void> {
+  await printHelloWorld()
+
+  // check that the getting started works with both spiritnet and peregine
+  const api_spirit = await connect_spirit()
+  try {
+    await fetchDidAndCredential(api_spirit)
+  } finally {
+    await disconnect()
+  }
+
+  const api_pere = await connect_pere()
+  try {
+    await fetchDidAndCredential(api_pere)
+  } finally {
+    await disconnect()
+  }
 }
