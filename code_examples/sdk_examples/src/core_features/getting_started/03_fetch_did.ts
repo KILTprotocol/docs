d6e27108bfee01075b2bb1cd605196d8a7ce092a
Chris Chinchilla <chris@kilt.io>
2024-05-17 15:06:30 +0200

    Revert "Change web3name"
    This reverts commit a5351da92420a39828771d622e4c567a57aff7ee.

--- code_examples/sdk_examples/src/core_features/getting_started/03_fetch_did.ts
+++ code_examples/sdk_examples/src/core_features/getting_started/03_fetch_did.ts
@@ -4,13 +4,13 @@ import * as Kilt from '@kiltprotocol/sdk-js'
 export async function main(): Promise<Kilt.DidUri | null> {
   let apiConfig = Kilt.ConfigService.get('api')
   const encodedJohnDoeDetails =
-    await apiConfig.call.did.queryByWeb3Name('kiltnerd123')
+    await apiConfig.call.did.queryByWeb3Name('john_doe')
 
   // This function will throw if johnDoeOwner does not exist
   const {
     document: { uri }
   } = Kilt.Did.linkedInfoFromChain(encodedJohnDoeDetails)
-  console.log(`My name is kiltnerd123 and this is my DID: "${uri}"`)
+  console.log(`My name is john_doe and this is my DID: "${uri}"`)
 
   return uri
 }
