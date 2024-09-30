940a7eb11d8b0e1cfc9662afeaf530e5466e253b
Chris Chinchilla <chris@kilt.io>
2024-09-30 12:38:41 +0200

    Draft
    Signed-off-by: Chris Chinchilla <chris@kilt.io>

--- code_examples/sdk_examples/src/workshop/index.ts
+++ code_examples/sdk_examples/src/workshop/index.ts
@@ -1,70 +1,77 @@
-import * as Kilt from '@kiltprotocol/sdk-js'
+// import type {
+//   KiltAddress,
+//   SignerInterface,
+//   KeyringPair,
+//   MultibaseKeyPair,
+//   TransactionSigner,
+//   ICType
+// } from "@kiltprotocol/types";
+// import { Keyring } from "@polkadot/keyring";
+// import { BN } from "@polkadot/util";
+import * as Kilt from "@kiltprotocol/sdk-js";
+// import { Multikey } from "@kiltprotocol/utils";
+// import { BalanceUtils, Blockchain } from "@kiltprotocol/chain-helpers";
+// import { CType } from "@kiltprotocol/credentials";
+import { mnemonicGenerate, randomAsU8a } from '@polkadot/util-crypto'
+
 
 import { attestingFlow } from './issuer/issueCredential'
-import { createFullDid } from './issuer/generateDid'
+import { createDid } from './issuer/generateDid'
 import { ensureStoredCtype } from './issuer/generateCtype'
 import { generateAccount } from './issuer/generateAccount'
-import { generateKeypairs as generateAttesterKeypairs } from './issuer/generateKeypairs'
-import { generateKeypairs as generateClaimerKeypairs } from './holder/generateKeypairs'
+// import { generateKeypairs as generateAttesterKeypairs } from './issuer/generateKeypairs'
+// import { generateKeypairs as generateClaimerKeypairs } from './holder/generateKeypairs'
 import { generateCredential } from './holder/generateCredential'
-import { generateLightDid } from './holder/generateLightDid'
+// import { generateLightDid } from './holder/generateLightDid'
 import { getFunds } from '../getFunds'
 import { verificationFlow } from './verify'
 
 export async function testWorkshop(
-  account: Kilt.KeyringPair,
-  wssAddress: string
 ) {
   console.log('Running the workshop!')
+// TODO: Move to env var
 
-  Kilt.ConfigService.set({ submitTxResolveOn: Kilt.Blockchain.IS_IN_BLOCK })
-  await Kilt.connect(wssAddress)
+  const api = await Kilt.connect("wss://peregrine.kilt.io");
+  console.log("connected");
+
+  // ┏━━━━━━━━━━━━┓
+  // ┃ Get funds  ┃
+  // ┗━━━━━━━━━━━━┛
+  //
+// TODO: Move to env var
+  const faucetAccount = Kilt.generateKeypair({
+    type: "sr25519",
+    seed: "0xe566550fec3ca23d80dfe9e9529ada463b93fc33f17219c1089de906f7253f1c",
+  });
+  const authenticationKeyPair = Kilt.generateKeypair({ type: "ed25519" });
+  const submitterAccount = Kilt.generateKeypair({
+    type: "ed25519",
+    seed: "frequent arrange trap mouse shove labor rookie bitter absent club field exhibit",
+  });
+  console.log("keypair generation complete");
+
+  await getFunds(faucetAccount, submitterAccount, 5);
+  console.log("Successfully transferred tokens");
+
+
 
   // Setup attester account.
-  const { account: attesterAccount } = await generateAccount()
+  // const { account: attesterAccount } = await generateAccount()
 
   // Setup claimer & create a credential.
-  const claimerMnemonic = Kilt.Utils.Crypto.mnemonicGenerate()
-  const { authentication } = generateClaimerKeypairs(claimerMnemonic)
-  const lightDid = generateLightDid(claimerMnemonic)
+  // const claimerMnemonic = mnemonicGenerate()
+  // const { authentication } = generateClaimerKeypairs(claimerMnemonic)
+  // const lightDid = generateLightDid(claimerMnemonic)
 
-  generateCredential(lightDid.uri, {
-    age: 27,
-    name: 'Karl'
-  })
+  // generateCredential(lightDid.uri, {
+  //   age: 27,
+  //   name: 'Karl'
+  // })
 
-  await getFunds(account, attesterAccount.address, 5)
+  // await getFunds(account, attesterAccount.address, 5)
 
   // Create attester DID & ensure CType.
-  const { fullDid: attesterDid } = await createFullDid(attesterAccount)
-  const { assertionMethod } = generateAttesterKeypairs()
-
-  await ensureStoredCtype(
-    attesterAccount,
-    attesterDid.uri,
-    async ({ data }) => ({
-      signature: assertionMethod.sign(data),
-      keyType: assertionMethod.type
-    })
-  )
-
-  // Do attestation & verification.
-  const credential = await attestingFlow(
-    lightDid.uri,
-    attesterAccount,
-    attesterDid.uri,
-    async ({ data }) => ({
-      signature: assertionMethod.sign(data),
-      keyType: assertionMethod.type
-    })
-  )
-  await verificationFlow(
-    credential,
-    async ({ data }) => ({
-      signature: authentication.sign(data),
-      keyType: authentication.type,
-      keyUri: `${lightDid.uri}${lightDid.authentication[0].id}`
-    }),
-    [attesterDid.uri]
-  )
+  const { fullDid: attesterDid } = await createDid(submitterAccount, authenticationKeyPair)
 }
+
+testWorkshop()
--- code_examples/sdk_examples/src/workshop/issuer/generateDid.ts
+++ code_examples/sdk_examples/src/workshop/issuer/generateDid.ts
@@ -1,60 +1,69 @@
-import * as Kilt from '@kiltprotocol/sdk-js'
-import { config as envConfig } from 'dotenv'
-import { generateAccount } from './generateAccount'
+import type {
+  KiltAddress,
+  SignerInterface,
+  KeyringPair,
+  MultibaseKeyPair,
+  TransactionSigner,
+  ICType,
+  DidDocument,
+} from "@kiltprotocol/types";
+import { Keyring } from "@polkadot/keyring";
+import { BN } from "@polkadot/util";
+import * as Kilt from "@kiltprotocol/sdk-js";
+import { Multikey } from "@kiltprotocol/utils";
+import { Blockchain, BalanceUtils } from "@kiltprotocol/chain-helpers";
+import { CType } from "@kiltprotocol/credentials";
+  // ┏━━━━━━━━━━━━┓
+  // ┃ create DID ┃
+  // ┗━━━━━━━━━━━━┛
+  //
+  // Generate the DID-signed creation tx and submit it to the blockchain with the specified account.
+  // The DID Document will have one Verification Key with an authentication relationship.
+  //
+  // Note the following parameters:
+  // - `api`: The connected blockchain api.
+  // - `signers`: The keys for verification materials inside the DID Document. For creating a DID,
+  // only the key for the authentication verification method is required.
+  // - `submitter`: The account used to submit the transaction to the blockchain. Note: the submitter account must have
+  // enough funds to cover the required storage deposit.
+  // - `fromPublicKey`: The public key that will feature as the DID's initial authentication method and will determine the DID identifier.
 
-export async function createFullDid(
-  creatorAccount: Kilt.KiltKeyringPair & {
-    type: 'ed25519' | 'sr25519' | 'ecdsa'
+  // Much like current workshop, these two accoutns can be the same
+  export async function createDid(  
+    submitterAccount: MultibaseKeyPair, 
+    authenticationKeyPair:MultibaseKeyPair) : 
+      Promise<{
+      fullDid: DidDocument
+    }> {
+      const api = Kilt.ConfigService.get('api')
+
+    const transactionHandler = Kilt.DidHelpers.createDid({
+    api,
+    signers: [authenticationKeyPair],
+    submitter: submitterAccount,
+    fromPublicKey: authenticationKeyPair.publicKeyMultibase,
+  });
+
+  // The `createDid` function returns a transaction handler, which includes two methods:
+  // - `submit`: Submits a transaction for inclusion in a block, resulting in its execution in the blockchain runtime.
+  // - `getSubmittable`: Produces transaction that can be submitted to a blockchain node for inclusion, or signed and submitted by an external service.
+
+  // Submit transaction.
+  // Note: `submit()` by default, waits for the block to be finalized. This behaviour can be overwritten
+  // in the function's optional parameters.
+  const didDocumentTransactionResult = await transactionHandler.submit();
+
+  // Once the transaction is submitted, the result should be checked.
+  // For the sake of this example, we will only check if the transaction went through.
+  if (didDocumentTransactionResult.status !== "confirmed") {
+    console.log(didDocumentTransactionResult.status);
+    throw new Error("create DID failed");
   }
-): Promise<{
-  fullDid: Kilt.DidDocument
-}> {
-  const api = Kilt.ConfigService.get('api')
 
-  const verificationMethod = Kilt.Did.publicKeyToChain(creatorAccount)
+  // Get the DID Document from the transaction result.
+  let { didDocument, signers } = didDocumentTransactionResult.asConfirmed;
 
-  const txs = [
-    api.tx.did.createFromAccount(verificationMethod),
-    api.tx.did.dispatchAs(
-      creatorAccount.address,
-      api.tx.did.setAttestationKey(verificationMethod)
-    )
-  ]
-
-  console.log('Creating DID from account…')
-  await Kilt.Blockchain.signAndSubmitTx(
-    api.tx.utility.batch(txs),
-    creatorAccount
-  )
-  const didUri = Kilt.Did.getFullDidUriFromKey(creatorAccount)
-  const encodedFullDid = await api.call.did.query(Kilt.Did.toChain(didUri))
-  const { document: didDocument } = Kilt.Did.linkedInfoFromChain(encodedFullDid)
-
-  if (!didDocument) {
-    throw new Error('Full DID was not successfully created.')
-  }
-
+  console.log("Did created");
+  // DID verify step currently
   return { fullDid: didDocument }
-}
-
-// Don't execute if this is imported by another file.
-if (require.main === module) {
-  ;(async () => {
-    envConfig()
-
-    try {
-      await Kilt.connect(process.env.WSS_ADDRESS as string)
-
-      // Load attester account
-      const accountMnemonic = process.env.ATTESTER_ACCOUNT_MNEMONIC as string
-      const { account } = generateAccount(accountMnemonic)
-      const { fullDid } = await createFullDid(account)
-
-      console.log('\nsave following to .env to continue\n')
-      console.error(`ATTESTER_DID_URI="${fullDid.uri}"\n`)
-    } catch (e) {
-      console.log('Error while creating attester DID')
-      throw e
-    }
-  })()
 }
