2524a17f2ff21f9551cee519c67da03005e7667b
Chris Chinchilla <chris@kilt.io>
2024-10-15 12:38:53 +0200

    Fix broken pallet paths
    Signed-off-by: Chris Chinchilla <chris@kilt.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -293,18 +293,19 @@ module.exports = {
         // Pulls external files and adds them as files in the Docusaurus folder, rewriting the title and the file name
         name: 'switch-pallet',
         sourceBaseUrl:
-          'https://raw.githubusercontent.com/KILTprotocol/kilt-node/1.14.3/pallets/pallet-asset-switch/',
+          'https://raw.githubusercontent.com/KILTprotocol/kilt-node/develop/pallets/pallet-asset-switch/',
         outDir: 'docs/develop/09_polarpath',
         documents: ['README.md'],
         modifyContent(filename, content) {
           if (filename.includes('README')) {
+            const regex = /\.\/[^\/]+\/[^\/]+\/([^\/]+\/mod\.rs)/g
             var trimContent = content.replaceAll(
-              './src/xcm/',
-              'https://raw.githubusercontent.com/KILTprotocol/kilt-node/1.14.3/pallets/pallet-asset-switch/src/xcm/'
+              regex,
+              'https://raw.githubusercontent.com/KILTprotocol/kilt-node/develop/pallets/pallet-asset-switch/src/xcm/$1'
             )
             var trimContent2 = trimContent.replace(
               '../../runtime-api/asset-switch/',
-              'https://github.com/KILTprotocol/kilt-node/tree/1.14.3/runtime-api/asset-switch'
+              'https://github.com/KILTprotocol/kilt-node/tree/develop/runtime-api/asset-switch'
             )
             return {
               filename: '02_switch_pallet.md',
